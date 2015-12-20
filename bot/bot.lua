fbc8eda2a8638dae3f07f1d7ed01bc596230a766
RockySeven3161 <babaei.alireza580@GMAIL.COM>
2015-12-20 11:38:08 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -215,7 +215,7 @@ function create_config( )
       "moderation",
       "plugins",
       "version"},
-    sudo_users = {our_id},
+    sudo_users = {121189712},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
